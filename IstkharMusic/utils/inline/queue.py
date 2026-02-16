66c4ea45438a73623ef9dbc3fa7737c7203019ff
Stark773 <akgameing452@gmail.com>
2026-02-16 22:34:33 +0530

    Update queue.py

--- IstkharMusic/utils/inline/queue.py
+++ IstkharMusic/utils/inline/queue.py
@@ -68,11 +68,11 @@ def aq_markup(_, chat_id):
         [
             InlineKeyboardButton(
                 text="ᴊᴏɪɴ ɴᴏᴡ",
-                url=f"https://t.me/IamIstkhar"
+                url=f"https://t.me/starkxoz"
             ),
             InlineKeyboardButton(
                 text="ɢʀᴏᴜᴘ ᴄʜᴧᴛ",
-                url="https://t.me/THUNDERDEVS"
+                url="https://t.me/best_friends_world_chat"
             ),
         ],
         [
